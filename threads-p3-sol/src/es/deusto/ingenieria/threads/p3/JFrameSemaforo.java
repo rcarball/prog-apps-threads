e60799d4e00758b5e182f4a95b89a05ac6ef4b61
Roberto Carballedo <roberto.carballedo@deusto.es>
2025-11-03 10:32:05 +0100

    Mejora del ejemplo del semáforo.

--- threads-p3-sol/src/es/deusto/ingenieria/threads/p3/JFrameSemaforo.java
+++ threads-p3-sol/src/es/deusto/ingenieria/threads/p3/JFrameSemaforo.java
@@ -17,6 +17,7 @@ import javax.swing.SwingUtilities;
 public class JFrameSemaforo extends JFrame {
     private static final long serialVersionUID = 1L;
 
+    // Componentes de la interfaz gráfica: labels y botones
     private JLabel lblLuzRoja = new JLabel("", JLabel.CENTER);
     private JLabel lblLuzAmarilla = new JLabel("", JLabel.CENTER);
     private JLabel lblLuzVerde = new JLabel("", JLabel.CENTER);
@@ -24,35 +25,48 @@ public class JFrameSemaforo extends JFrame {
     private JButton btnPausar = new JButton("Pausar");
     private JButton btnDetener = new JButton("Detener");
 
+    // Hilo que gestiona el funcionamiento del semáforo
     private HiloSemaforo hiloSemaforo;
+    // Mapa para asociar las fases del semáforo a los labels
     private Map<Fase, JLabel> mapaLabels;
+    // Variable para controlar si el semáforo está en pausa o no
     private boolean enPausa = false;
 
+    // Enumeración para adefinir las fases del semáforo
+    // con su duración y color asociados
     public enum Fase {
-        VERDE(10), AMARILLO(3), ROJO(10);
+        VERDE(10, Color.GREEN), 
+        AMARILLO(3, Color.YELLOW), 
+        ROJO(10, Color.RED);
 
         private int duracion;
+        private Color color;
 
-        Fase(int duracion) {
+        Fase(int duracion, Color color) {
             this.duracion = duracion;
+            this.color = color;
         }
 
         public int getDuracion() {
             return duracion;
         }
+        
+        public Color getColor() {
+			return color;
+		}
     }
 
     public JFrameSemaforo() {
-        // Se inicializa el mapa de labels del semáforo
+        // Se inicializa el mapa de fases y labels
         this.mapaLabels = new HashMap<>();
         this.mapaLabels.put(Fase.VERDE, lblLuzVerde);
         this.mapaLabels.put(Fase.AMARILLO, lblLuzAmarilla);
         this.mapaLabels.put(Fase.ROJO, lblLuzRoja);
 
         // Se configuran los labels
-        this.configurarLabel(lblLuzRoja, Color.RED);
-        this.configurarLabel(lblLuzVerde, Color.GREEN);
-        this.configurarLabel(lblLuzAmarilla, Color.YELLOW);
+        this.configurarLabel(lblLuzRoja, Fase.ROJO);
+        this.configurarLabel(lblLuzVerde, Fase.VERDE);
+        this.configurarLabel(lblLuzAmarilla, Fase.AMARILLO);
 
         // Se definen los listeners de los botones
         btnIniciar.addActionListener(e -> iniciar());
@@ -83,10 +97,10 @@ public class JFrameSemaforo extends JFrame {
         setLocationRelativeTo(null);
     }
 
-    private void configurarLabel(JLabel label, Color color) {
+    private void configurarLabel(JLabel label, Fase fase) {
         label.setPreferredSize(new Dimension(100, 100));
         label.setBackground(Color.BLACK);
-        label.setForeground(color);
+        label.setForeground(fase.getColor());
         label.setFont(new Font("Courier", Font.BOLD, 72));
         label.setOpaque(true);
     }
@@ -110,7 +124,8 @@ public class JFrameSemaforo extends JFrame {
     }
 
     private void pausar() {
-    	// Se usa el bloque synchronized sobre el hiloSemaforo para reactivarlo
+    	// Se usa el bloque synchronized para asegurar la exclusión mutua
+    	// al modificar la variable enPausa y notificar el hilo, si es necesario
         synchronized (hiloSemaforo) {
             enPausa = !enPausa;
 
@@ -119,12 +134,13 @@ public class JFrameSemaforo extends JFrame {
             } else {
                 btnPausar.setText("Pausar");
                 // Se notifica al hilo para que se reanude
-                hiloSemaforo.notifyAll();
+                hiloSemaforo.notify();
             }
         }
     }
 
     private void detener() {
+    	// Se comprueba que el hilo no sea nulo: ha sido iniciado
         if (hiloSemaforo != null) {
         	// Se interrumpe el hilo
             hiloSemaforo.interrupt();
@@ -144,36 +160,41 @@ public class JFrameSemaforo extends JFrame {
     private class HiloSemaforo extends Thread {
         @Override
         public void run() {
-            // Secuencia de fases del semáforo
+            // Array de fases para iterar sobre el de manera continua
             Fase[] fases = {Fase.VERDE, Fase.AMARILLO, Fase.ROJO};
             // Variable auxiliar para controlar la fase actual
             int indiceFases = 0;
 
             // Mientras el hilo no haya sido interrumpido
             while (!this.isInterrupted()) {
-                // Obtener label activo a partir del color de la fase
+                // Obtener label activo a partir del la fase actual
                 JLabel lblActivo = mapaLabels.get(fases[indiceFases]);
                 // Obtener duracion de la fase
                 int duracion = fases[indiceFases].getDuracion();
 
                 // Se inicia la cuenta atrás
                 for (int i = duracion; i > 0; i--) {
-                    // Se usa el bloque synchronized por si se quiere pausar el hilo
+                    // Se usa el bloque synchronized para pausar el hilo
                     synchronized (this) {
-                    	// Mientras el hilo esté en pausa se espera
-                    	while (enPausa) {
+                    	// Si el semáforo está en pausa, se espera
+                    	// Aquí de puede usar tanto un if como un while
+                    	if (enPausa) {
                             try {
                             	// Se espera a que se reanude el hilo
                                 this.wait();
                             } catch (InterruptedException e) {
-                                // Si se intenta interrumpir durante la pausa, se sale del bucle
+                                // si se produce una excepcion, se interrumpe el hilo                            	
                             	this.interrupt();
+                            	// Se sale del bucle que cuenta los segundos
                                 break;
                             }
                         }
                     }
 
-                    // Variable final para usar en la lambda
+                    // Variable final para usar en la expresión lambda
+                    // es necesario porque no se puede utilizar variables
+                    // que cambien de valor dentro o tras pasar por una lambda
+                    // (como es el caso de i en este bucle)
                     final int tiempoRestante = i;
 
                     // Se actualiza el Label activo con el tiempo restante
@@ -199,7 +220,7 @@ public class JFrameSemaforo extends JFrame {
                 });
 
                 // Se obtiene el índice de la siguiente fase
-                indiceFases = (indiceFases + 1) % Fase.values().length;
+                indiceFases = (indiceFases + 1) % fases.length;
             }
         }
     }
